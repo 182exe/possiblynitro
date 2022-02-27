71a05967f7e70f10570574dbecd85700535c5f44
182exe <81325987+182exe@users.noreply.github.com>
2022-02-27 10:51:45 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lowercase = 'abcdefghijklmnop';
+const number = '0123456789'
+
+var settingLength = 16;
+var settingTime = 3;
+
+function generateString(length) {
+  let result = '';
+  const uppercaseLength = uppercase.length;
+  const lowercaseLength = lowercase.length;
+  const numberLength = number.length;
+  for ( let i = 0; i < length; i++ ) {
+    result += 'https://discord.gift/';
+    for ( let i = 0; i < 16; i++ ) {
+      let randInt = Math.floor(Math.random() * 3);
+      if (randInt === 0) {
+        result += uppercase.charAt(Math.floor(Math.random() * uppercaseLength));
+      } else {
+        if (randInt === 1) {
+          result += lowercase.charAt(Math.floor(Math.random() * lowercaseLength));
+        } else {
+          if (randInt === 2) {
+            result += number.charAt(Math.floor(Math.random() * numberLength));
+          }
+        }
+      }
+    }
+    result += '\n';
+  }
+  return result;
+}
+
+function start(lengthsetting) {
+  const raw = (generateString(lengthsetting));
+  const str = raw;
+  const text = document.querySelector(".text");
+  const input = document.querySelector(".input");
+  
+  function populateText(str) {
+    str.split("").map(letter => {
+      const span = document.createElement("span");
+      span.innerText = letter;
+      text.appendChild(span);
+    });
+  }
+  populateText(str);
+}
+
+function updateSettings() {
+  if (document.getElementById("lengthInput").value.length == 0) {
+    window.alert('Please input values in the setting boxes before generating a level.')
+  } else {
+    var settingLength = document.getElementById("lengthInput").value;
+    start(settingLength);
+  }
+}
